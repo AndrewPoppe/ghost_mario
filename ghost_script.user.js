574a2a96c184681e535e4d91ff8412d77c834298
ballparts <poppe076@gmail.com>
2015-02-06 16:51:19 -0700

    replaced animation method with non-dumb one

--- ghost_script.user.js
+++ ghost_script.user.js
@@ -26,33 +26,33 @@ function createNullArray(N) {
 // it compares the best time this round to the previously saved best time,
 // and if this round's time is better it replaces the data there
 function storeData(positions, id, name, time) {
-	
-	// get currently stored best time
-	currentBest = GM_getValue('currentBestData');
-	
-	// compare this game's best to stored currently stored best time
-	// if this game's is better, store this game's instead
-	if(typeof currentBest === 'undefined' || JSON.parse(currentBest).time >= time) {
-		winnerData = {
+    
+    // get currently stored best time
+    currentBest = GM_getValue('currentBestData');
+    
+    // compare this game's best to stored currently stored best time
+    // if this game's is better, store this game's instead
+    if(typeof currentBest === 'undefined' || JSON.parse(currentBest).time >= time) {
+        winnerData = {
             positions : positions['player'+id],
             time : time
-		};
-		
-		GM_setValue('currentBestData', JSON.stringify(winnerData));
-	};
-
+        };
+        
+        GM_setValue('currentBestData', JSON.stringify(winnerData));
+    };
+    
 }
 
 // function to start recording
 function recordGhostData() {
-	var positions = {};			// this object holds all position data 
-	var fps = 60;				// how frequently we record player position data (e.g., 60 times per second)
-	var saveDuration = 180;		// save data for maximum of 3 minutes
-	
-	// function to save game data
+    var positions = {};			// this object holds all position data 
+    var fps = 60;				// how frequently we record player position data (e.g., 60 times per second)
+    var saveDuration = 180;		// save data for maximum of 3 minutes
+    
+    // function to save game data
     saveGhostData = function () {
-    	
-    	// create player objects within positions objects for any player not already represented
+        
+        // create player objects within positions objects for any player not already represented
         var currentPlayers = tagpro.players;
         for (player in currentPlayers) {
             if (!positions['player' + player]) {
@@ -71,7 +71,7 @@ function recordGhostData() {
         // loop through those player attributes, shift each array, and append new values onto array
         // this is what actually records the data
         for (player in positions) {
-        	for (i in positions[player]) {
+            for (i in positions[player]) {
                 positions[player][i].shift();
                 positions[player][i].push(typeof tagpro.players[player.replace('player', '')] != 'undefined' ? tagpro.players[player.replace('player', '')][i] : null);
             };
@@ -80,12 +80,12 @@ function recordGhostData() {
     
     // interval for calling saveGameData function at proper frequency
     var recordInterval = setInterval(saveGhostData, 1000 / fps);
-
-	// set up listener for mario grabbed event
-	// this will fire the storeData function and stop the saveGameData interval
-	tagpro.socket.on('mario', function (mario) {
-    	storeData(positions, mario.id, mario.name, mario.time);
-    	clearInterval(recordInterval);
+    
+    // set up listener for mario grabbed event
+    // this will fire the storeData function and stop the saveGameData interval
+    tagpro.socket.on('mario', function (mario) {
+        storeData(positions, mario.id, mario.name, mario.time);
+        clearInterval(recordInterval);
     });
     
 };
@@ -99,32 +99,32 @@ function recordGhostData() {
 
 // function to get stored data and clean it.
 function getStoredData() {
-	var dat = GM_getValue('currentBestData');
-	if(!dat) return
+    var dat = GM_getValue('currentBestData');
+    if(!dat) return
     dat = JSON.parse(dat);
     if(!dat.positions) { GM_deleteValue('currentBestData'); return}
-	for(var i=0; i < dat.positions.x.length; i++) {
-		if(dat.positions.x[i] === null) {
-			dat.positions.x.shift();
-			dat.positions.y.shift();
-			i--
-		}
-	}
-	return(dat)
+    for(var i=0; i < dat.positions.x.length; i++) {
+        if(dat.positions.x[i] === null) {
+            dat.positions.x.shift();
+            dat.positions.y.shift();
+            i--
+        }
+    }
+    return(dat)
 }
 
 // function to start ghost animation
-function animateGhost(dat) {
-	if(!dat) return
-	var i = 0;
-	interval = setInterval(function(){
-		tagpro.renderer.drawSpawn(dat.positions.x[i],dat.positions.y[i],0,1000/60);
-		tagpro.renderer.drawSpawn(dat.positions.x[i],dat.positions.y[i],0,1000/60);
-		i++;
-		if(i >= dat.positions.x.length) clearInterval(interval);
-	}, 1000/60)
+function animateGhost(dat, ghost) {
+    if(!dat) return
+    var i = 0;
+    interval = setInterval(function(){
+        ghost.position.x = dat.positions.x[i];
+        ghost.position.y = dat.positions.y[i];
+        i++;
+        if(i >= dat.positions.x.length) clearInterval(interval);
+    }, 1000/60)
 }
-		
+
 
 
 //--------------------------------------------//
@@ -135,11 +135,18 @@ function animateGhost(dat) {
 $(document).ready(function() {
     tagpro.ready(function() {
         var dat = getStoredData();
+        var ghost = tagpro.tiles.draw(tagpro.renderer.layers.foreground, 
+                                      "blueball", 
+                                      {x: dat.positions.x[0], y: dat.positions.x[0]}, 
+                                      null, 
+                                      null, 
+                                      0.75, 
+                                      true);
         tagpro.socket.on('time', function(time) {
             if(time.state === 1) {
                 recordGhostData();
-				animateGhost(dat);
-			};
-		});
-	});
+                animateGhost(dat, ghost);
+            };
+        });
+    });
 });
